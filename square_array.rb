3d75b86879c4d4f388112640731a35ecbc6689c2
JillKlatt <klatt.jill@gmail.com>
2021-02-03 14:12:15 -0600

    commit tree c767b4a0ef633ca4532a1384b4e63d7e8eaa3c3b

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+require "pry"
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |original_num|
+    new_array << original_num ** 2
+    #binding.pry
+  end
+  new_array
 end
